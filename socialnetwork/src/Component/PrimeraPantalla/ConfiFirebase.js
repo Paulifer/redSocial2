08c85877da3a5846bc7f9ae3616772bb4bb6a8a0
Paulifer <pauli.hua.timm@gmail.com>
2018-09-27 02:05:19 -0300

    añadiendo firebase

---
+++ socialnetwork/src/Component/PrimeraPantalla/ConfiFirebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+    firebase.initializeApp({
+    apiKey: "AIzaSyDQN-oRt1cBUErsVaKvBSQIwjdInoTXjRg",
+    authDomain: "redsocial2-b859e.firebaseapp.com",
+    databaseURL: "https://redsocial2-b859e.firebaseio.com",
+    projectId: "redsocial2-b859e",
+    storageBucket: "redsocial2-b859e.appspot.com",
+    messagingSenderId: "564097347611"
+});
+
+export default firebase;
